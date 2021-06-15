3aa3f36076c94bc514b4999c72805e30dbb57064
felipevargas-bz <felipe15angel@gmail.com>
2021-06-15 14:38:14 -0500

    TASK0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	while (i_index < size)
 	{
 		j_index = 0;
-		while (j_index < ((size - i_index) - 1))
+		while (j_index < (size - i_index))
 		{
 			if (array[j_index] > array[j_index + 1])
 			{
